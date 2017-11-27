0a910ed939bbdc8d945112ef73a5d1eef3d3b742
GwonHyeok <me@ghyeok.io>
2017-11-27 16:08:07 +0900

    openapi 스토어 검색 추가

--- modules/openApi/restClient/searchClient.js
+++ modules/openApi/restClient/searchClient.js
@@ -3,7 +3,8 @@ class SearchClient {
   constructor(openApiRestClient) {
     this.openApiRestClient = openApiRestClient;
     this.endPoints = {
-      blog: '/search/blog.json'
+      blog: '/search/blog.json',
+      shop: '/search/shop.json'
     }
   }
 
@@ -14,6 +15,19 @@ class SearchClient {
         {
           method: 'GET',
           endPoint: this.endPoints.blog,
+          qs: { query: keyword, display, start }
+        }
+      )
+    );
+  }
+
+  shop(keyword, start = 1, display = 10) {
+    return this.openApiRestClient.request(
+      Object.assign(
+        {},
+        {
+          method: 'GET',
+          endPoint: this.endPoints.shop,
           qs: { query: keyword, display, start }
         }
       )
